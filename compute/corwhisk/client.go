85bc240caaeff9e8c3f24c8dd6b2fd240eb36c09
tgoldboom <tgoldboom@googlemail.com>
2022-06-28 12:54:30 +0200

    memory and poll logging added

--- compute/corwhisk/client.go
+++ compute/corwhisk/client.go
@@ -46,6 +46,7 @@ type WhiskClient struct {
 	multiDeploymentFeature bool
 	hintingFeature         bool
 	deployments            map[string][]WhiskFunctionConfig
+	memory                 int
 
 	metrics *api.Metrics
 	sync.Mutex
@@ -298,6 +299,8 @@ func (l *WhiskClient) DeployFunction(conf WhiskFunctionConfig) error {
 		return err
 	}
 
+	l.memory = conf.Memory
+
 	if !l.multiDeploymentFeature {
 		return l.deployFunction(conf, buildPackage, codeHashDigest)
 	} else {
@@ -452,7 +455,7 @@ func (l *WhiskClient) fetchActivationMetrics(id string) {
 			return
 		}
 
-		l.collectInvocation(invoke, time.Now().UnixMilli())
+		l.collectInvocation(invoke, time.Now().UnixMilli(), 0)
 
 	}
 
@@ -547,7 +550,7 @@ func (l *WhiskClient) PollActivation(activationID string) (io.ReadCloser, error)
 		} else if response.StatusCode == 200 {
 			log.Debugf("polled %s successfully", activationID)
 			l.spawn.TryAllow()
-			l.collectInvocation(invoke, time.Now().UnixMilli())
+			l.collectInvocation(invoke, time.Now().UnixMilli(), x)
 			marshal, err := json.Marshal(invoke.Result)
 			if err == nil {
 				return ioutil.NopCloser(bytes.NewReader(marshal)), nil
@@ -559,7 +562,7 @@ func (l *WhiskClient) PollActivation(activationID string) (io.ReadCloser, error)
 	return nil, fmt.Errorf("could not fetch activation after %d ties in %d", MaxPullRetries, backoff+backoff-1)
 }
 
-func (l *WhiskClient) collectInvocation(invoke *whisk.Activation, rend int64) {
+func (l *WhiskClient) collectInvocation(invoke *whisk.Activation, rend int64, prematurePolls int) {
 	if l.metrics != nil {
 		l.metrics.Collect(map[string]interface{}{
 			"RId":    invoke.ActivationID,
@@ -568,6 +571,8 @@ func (l *WhiskClient) collectInvocation(invoke *whisk.Activation, rend int64) {
 			"eLat":   invoke.End - invoke.Start,
 			"rEnd":   rend,
 			"dLat":   rend - invoke.End,
+			"pPolls": prematurePolls,
+			"memory": l.memory,
 		})
 	}
 }
