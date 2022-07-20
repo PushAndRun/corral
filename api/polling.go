13fb38624da7eb9ca43fa4c8e9dbff54a33aa46d
PushAndRun <tgoldboom@gmail.com>
2022-07-20 15:53:20 +0200

    Added polling strategy to polling interface

--- api/polling.go
+++ api/polling.go
@@ -9,6 +9,8 @@ type JobInfo struct {
 	JobId int
 	//Number of the Query to associate the records with the experiment
 	QueryID int
+	//Polling algorithm that was used
+	PollingStrategy string
 	//Total number of Inputs for this job
 	Splits int
 	//SplitSize of this job in byte
