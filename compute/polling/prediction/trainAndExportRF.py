238c5984a3c8d2044ba3691bafcec0bfb0ae7685
PushAndRun <tgoldboom@gmail.com>
2022-08-17 15:12:33 +0200

    Reexported RF

--- compute/polling/prediction/trainAndExportRF.py
+++ compute/polling/prediction/trainAndExportRF.py
@@ -129,13 +129,6 @@ MODEL_DIR = "pollingRF"
 export_path = os.path.join(MODEL_DIR, str(version))
 print('export_path = {}\n'.format(export_path))
 
-tf.keras.models.save_model(
-    model_7,
-    export_path,
-    overwrite=True,
-    include_optimizer=True,
-    save_format=None,
-    signatures=None,
-    options=None
-)
+model_7.save(export_path, overwrite=True)
+
 
